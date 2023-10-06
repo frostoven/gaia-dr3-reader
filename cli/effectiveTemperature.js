4f08f11583fe331ad57fed44320456eda2e63622
aggregate1166877 <noreply@frostoven.com>
2023-10-07 03:28:29 +1300

    Add effective temperature calculator.

---
+++ cli/effectiveTemperature.js
@@ -0,0 +1,33 @@
+function lumToEffectiveTemperature(luminosity) {
+  // Stefan-Boltzmann constant in W*m^-2*K^-4
+  const sigma = 5.67e-8;
+
+  // Solar constants
+  const solarLuminosity = 3.828e26; // in Watts
+  const solarRadius = 6.9634e8; // in Meters
+
+  // Convert input luminosity to Watts
+  const starLuminosity = luminosity * solarLuminosity;
+
+  // Calculate effective temperature
+  return Math.pow(starLuminosity / (4 * Math.PI * Math.pow(solarRadius, 2) * sigma), 0.25);
+}
+
+function effectiveTemperatureToLum(temperature) {
+  // Stefan-Boltzmann constant in W m^-2 K^-4
+  const sigma = 5.67e-8;
+
+  // Solar constants
+  const solarLuminosity = 3.828e26; // in Watts
+  const solarRadius = 6.9634e8; // in Meters
+
+  // Compute luminosity
+  return (
+    4 * Math.PI * Math.pow(solarRadius, 2) * sigma * Math.pow(temperature, 4)
+  ) / solarLuminosity;
+}
+
+export {
+  lumToEffectiveTemperature,
+  effectiveTemperatureToLum,
+};
